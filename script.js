768325a1d3a9d958e70209f8bedc05f11e716701
vytarask <vytaras.klimavicius@gmail.com>
2022-12-06 18:43:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const muzikantai = 5;
+
+if (muzikantai <= 0){
+    console.log('ne grupe');
+}
+else if (muzikantai == 1){
+    console.log('solo');
+}
+else if (muzikantai == 2){
+    console.log('duetas');
+}
+else if (muzikantai == 3){
+    console.log('trio');
+}
+else if (muzikantai == 4){
+    console.log('kvartetas');
+}
+else if (muzikantai >= 5) {
+    console.log('didele grupe');
+}
+else {
+    console.log('klaida');
+}
